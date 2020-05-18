5927d354df9399c3ee302f4b8d6cdd2d4b21b513
neel216 <neelr216@gmail.com>
2020-05-18 16:11:15 -0400

    added command line tools for Flask

--- server.py
+++ server.py
@@ -1,25 +1,5 @@
-from flask import Flask, request, jsonify, abort
+from ardensi import application
 
+app = application.app
 
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def hello():
-    return 'Hello world!', 200
-
-@app.route('/testing', methods=['POST'])
-def test():
-    req = request.json
-    if req is None or req['username'] is None or req['message'] is None:
-        return 'Could not handle that request.', 403
-    user = req['username']
-    message = req['message']
-
-    ret = {
-        'body': f'{user} said - {message} - at this time.'
-    }
-    return jsonify(ret), 200
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# connect to db here?
