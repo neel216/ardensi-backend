8e654698e5023c30b4a20f879ef92c3d8ea284ea
neel216 <neelr216@gmail.com>
2020-05-28 02:57:33 -0400

    added verified list of college email domains and their university name

--- ardensi/config.py
+++ ardensi/config.py
@@ -4,3 +4,8 @@ Loads config from environment variables
 import os
 
 ENV_DEV = 'development' == os.getenv('FLASK_ENV', 'production')
+DOMAINS = ['unc.edu', 'duke.edu']
+COLLEGES = {
+    'unc.edu': 'UNC-Chapel Hill',
+    'duke.edu': 'Duke University'
+}
